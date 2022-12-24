e5ecae85a15ef6ce3caac61f5472da0d35c354c1
Max <47116192+yddex@users.noreply.github.com>
2022-12-24 03:18:06 +0300

    Update DB_init.php

--- db/DB_init.php
+++ db/DB_init.php
@@ -23,4 +23,5 @@ $db->exec("CREATE TABLE comments (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     body TEXT NOT NULL
+    FOREIGN KEY (post_id) REFERENCES posts (id)
 );");
